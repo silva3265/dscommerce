cdef094526e7085999916b4666ba274492edfd41
Rômulo <silva3265@gmail.com>
2022-09-12 21:27:15 -0300

    Database seeding

--- src/main/java/com/devsuperior/descommerce/entities/OrderItem.java
+++ src/main/java/com/devsuperior/descommerce/entities/OrderItem.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_ordem_item")
+@Table(name = "tb_order_item")
 public class OrderItem {
 
     @EmbeddedId
